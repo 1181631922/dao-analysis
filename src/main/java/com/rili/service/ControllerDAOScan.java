cebfe5b08b20257374e58d883fa69c100165a1fb
fanyafeng <fanyafeng@365rili.com>
2017-03-14 14:09:48 +0800

    修改

--- src/main/java/com/rili/service/ControllerDAOScan.java
+++ src/main/java/com/rili/service/ControllerDAOScan.java
@@ -29,7 +29,7 @@ public class ControllerDAOScan {
             try {
                 FileInputStream fileInputStream = new FileInputStream(fullFilePath);
                 CompilationUnit compilationUnit = JavaParser.parse(fileInputStream);
-                new MethodChangerVisitor().visit(compilationUnit, null);
+                new MethodChangerVisitor(fileName).visit(compilationUnit, null);
             } catch (FileNotFoundException e) {
                 LOGGER.error("FileNotFoundException:{}", e);
             }
@@ -37,8 +37,17 @@ public class ControllerDAOScan {
     }
 
     private static class MethodChangerVisitor extends VoidVisitorAdapter<Void> {
+
+        private String fileName;
+
+        public MethodChangerVisitor(String fileName) {
+            this.fileName = fileName;
+        }
+
         @Override
         public void visit(MethodDeclaration n, Void arg) {
+//            System.out.println("---" + n.getNameAsString());
+            String methodName = n.getNameAsString();
             List<String> stringList = Arrays.asList(n.getBody().toString().split(";"));
             for (int i = 0; i < stringList.size(); i++) {
                 String node = stringList.get(i).replace("\" + \"", "").replace("\" +", "").replace("+ \"", "").toLowerCase();
@@ -49,31 +58,27 @@ public class ControllerDAOScan {
                     String simpleTable = node.substring(simpleStart, simpleEnd);
                     if (simpleTable.contains(" ")) {
                         String currentTable = simpleTable.substring(0, simpleTable.indexOf(" "));
-//                        System.out.println("  |---insert->" + currentTable);
+                        System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:insert");
                     } else if (simpleTable.contains("(")) {
                         String currentTable = simpleTable.substring(0, simpleTable.indexOf("("));
-//                        System.out.println("  |---insert->" + currentTable);
+                        System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:insert");
                     }
                 } else if (node.contains("update ")) {//改
-//                    System.out.println(node);
                     String currentTable = node.substring(node.indexOf("update ") + 6, node.indexOf("set") - 1).trim();
-//                    System.out.println("  |---update->" + currentTable);
+                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:update");
                 } else if (node.contains("delete from ")) {//删
-//                    System.out.println(node);
                     String currentTable = node.substring(node.indexOf("delete from ") + 12, node.indexOf("where"));
-//                    System.out.println("  |---delete->" + currentTable);
+                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:delete");
                 } else if (node.contains("select") && node.contains("where")) {//有where的查
-//                    System.out.println(node);
                     List<String> simpleTableList = Arrays.asList(node.split("from "));
                     for (int j = 0; j < simpleTableList.size(); j++) {
-//                        System.out.println(simpleTableList.get(j));
                         if (j > 0) {
                             String simpleTable = simpleTableList.get(j).trim();
                             if (!simpleTable.contains("select")) {
                                 if (j == simpleTableList.size() - 1 && simpleTable.contains(")") && !simpleTable.contains(" ")) {
-                                    System.out.println(simpleTable.substring(0, simpleTable.indexOf(")")));
+                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTable.substring(0, simpleTable.indexOf(")")) + ";操作:select");
                                 } else {
-                                    System.out.println(simpleTable.substring(0, simpleTable.indexOf(" ")));
+                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTable.substring(0, simpleTable.indexOf(" ")) + ";操作:select");
                                 }
                             }
 
@@ -87,10 +92,10 @@ public class ControllerDAOScan {
                                             List<String> groupList = Arrays.asList(selectGroupTable.split(","));
                                             for (int h = 0; h < groupList.size(); h++) {
                                                 String selectItemTable = groupList.get(h);
-                                                System.out.println("2222222222" + selectItemTable);
+                                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectItemTable + ";操作:select");
                                             }
                                         } else {
-                                            System.out.println("333333333" + selectGroupTable);
+                                            System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectGroupTable + ";操作:select");
                                         }
                                     } else {
                                         String selectGroupTable = simpleJoinTable.substring(0, simpleJoinTable.indexOf(" "));
@@ -98,10 +103,10 @@ public class ControllerDAOScan {
                                             List<String> groupList = Arrays.asList(selectGroupTable.split(","));
                                             for (int h = 0; h < groupList.size(); h++) {
                                                 String selectItemTable = groupList.get(h);
-                                                System.out.println("44444444" + selectItemTable);
+                                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectItemTable + ";操作:select");
                                             }
                                         } else {
-                                            System.out.println("55555555555" + selectGroupTable);
+                                            System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+selectGroupTable+";操作:select");
                                         }
                                     }
                                 }
@@ -115,24 +120,22 @@ public class ControllerDAOScan {
                         String simpleTable = sqlString.substring(0, sqlString.indexOf("\"")).trim();
                         if (simpleTable.contains(" ")) {
                             String currentTable = simpleTable.substring(0, simpleTable.indexOf(" "));
-                            System.out.println("  |---select->" + currentTable);
                             if (currentTable.contains(",")) {
                                 List<String> currentTableList = Arrays.asList(currentTable.split(","));
                                 for (int h = 0; h < currentTableList.size(); h++) {
-                                    System.out.println("11111111111" + currentTableList.get(h));
+                                    System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+currentTableList.get(h)+";操作:select");
                                 }
                             } else {
-                                System.out.println("2222222222" + currentTable);
+                                System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+currentTable+";操作:select");
                             }
                         } else {
-                            System.out.println("  |---select->" + simpleTable);
                             if (simpleTable.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(simpleTable.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("33333333" + simpleTableList.get(h));
+                                    System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+simpleTableList.get(h)+";操作:select");
                                 }
                             } else {
-                                System.out.println("44444444444" + simpleTable);
+                                System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+simpleTable+";操作:select");
                             }
                         }
                     } else {
@@ -142,19 +145,19 @@ public class ControllerDAOScan {
                             if (currentString.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(currentString.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("555" + simpleTableList.get(h));
+                                    System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+simpleTableList.get(h)+";操作:select");
                                 }
                             } else {
-                                System.out.println("66666" + sqlTrimString);
+                                System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+sqlTrimString+";操作:select");
                             }
                         } else {
                             if (sqlTrimString.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(sqlTrimString.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("77777" + simpleTableList.get(h));
+                                    System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+simpleTableList.get(h)+";操作:select");
                                 }
                             } else {
-                                System.out.println("88888" + sqlTrimString);
+                                System.out.println("文件名:"+fileName+";方法名:"+methodName+";数据库表名:"+sqlTrimString+";操作:select");
                             }
                         }
                     }
