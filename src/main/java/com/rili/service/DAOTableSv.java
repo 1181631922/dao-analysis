a6361731ee8e0302bc22f2312e848a71c0f8e6c7
fanyafeng <fanyafeng@365rili.com>
2017-03-17 15:55:23 +0800

    修改log

--- src/main/java/com/rili/service/DAOTableSv.java
+++ src/main/java/com/rili/service/DAOTableSv.java
@@ -68,32 +68,30 @@ public class DAOTableSv {
 
         @Override
         public void visit(MethodDeclaration n, Void arg) {
-//            System.out.println("---" + n.getNameAsString());
             String methodName = n.getNameAsString();
             List<String> stringList = Arrays.asList(n.getBody().toString().split(";"));
             for (int i = 0; i < stringList.size(); i++) {
                 String node = stringList.get(i).replace("\" + \"", "").replace("\" +", "").replace("+ \"", "").toLowerCase();
                 if (node.contains("insert into")) {//增
-//                    System.out.println(node);
                     int simpleStart = node.indexOf("into ") + 5;
                     int simpleEnd = node.indexOf(",");
                     String simpleTable = node.substring(simpleStart, simpleEnd);
                     if (simpleTable.contains(" ")) {
                         String currentTable = simpleTable.substring(0, simpleTable.indexOf(" "));
-                        System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:insert");
+                        LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTable+",operation:"+INSERT);
                         insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTable, INSERT));
                     } else if (simpleTable.contains("(")) {
                         String currentTable = simpleTable.substring(0, simpleTable.indexOf("("));
-                        System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:insert");
+                        LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTable+",operation:"+INSERT);
                         insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTable, INSERT));
                     }
                 } else if (node.contains("update ")) {//改
                     String currentTable = node.substring(node.indexOf("update ") + 6, node.indexOf("set") - 1).trim();
-                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:update");
+                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTable+",operation:"+UPDATE);
                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTable, UPDATE));
                 } else if (node.contains("delete from ")) {//删
                     String currentTable = node.substring(node.indexOf("delete from ") + 12, node.indexOf("where"));
-                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:delete");
+                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTable+",operation:"+DELETE);
                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTable, DELETE));
                 } else if (node.contains("select") && node.contains("where")) {//有where的查
                     List<String> simpleTableList = Arrays.asList(node.split("from "));
@@ -102,10 +100,10 @@ public class DAOTableSv {
                             String simpleTable = simpleTableList.get(j).trim();
                             if (!simpleTable.contains("select")) {
                                 if (j == simpleTableList.size() - 1 && simpleTable.contains(")") && !simpleTable.contains(" ")) {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTable.substring(0, simpleTable.indexOf(")")) + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTable.substring(0, simpleTable.indexOf(")"))+",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTable.substring(0, simpleTable.indexOf(")")), SELECT));
                                 } else {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTable.substring(0, simpleTable.indexOf(" ")).replace("(", "") + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTable.substring(0, simpleTable.indexOf(" ")).replace("(", "") +",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTable.substring(0, simpleTable.indexOf(" ")).replace("(", ""), SELECT));
                                 }
                             }
@@ -120,11 +118,11 @@ public class DAOTableSv {
                                             List<String> groupList = Arrays.asList(selectGroupTable.split(","));
                                             for (int h = 0; h < groupList.size(); h++) {
                                                 String selectItemTable = groupList.get(h);
-                                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectItemTable + ";操作:select");
+                                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+selectItemTable +",operation:"+SELECT);
                                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, selectItemTable, SELECT));
                                             }
                                         } else {
-                                            System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectGroupTable + ";操作:select");
+                                            LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+selectGroupTable +",operation:"+SELECT);
                                             insertTableBeanSet.add(new InsertTableBean(fileName, methodName, selectGroupTable, SELECT));
                                         }
                                     } else {
@@ -133,11 +131,11 @@ public class DAOTableSv {
                                             List<String> groupList = Arrays.asList(selectGroupTable.split(","));
                                             for (int h = 0; h < groupList.size(); h++) {
                                                 String selectItemTable = groupList.get(h);
-                                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectItemTable + ";操作:select");
+                                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+selectItemTable +",operation:"+SELECT);
                                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, selectItemTable, SELECT));
                                             }
                                         } else {
-                                            System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + selectGroupTable + ";操作:select");
+                                            LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+selectGroupTable +",operation:"+SELECT);
                                             insertTableBeanSet.add(new InsertTableBean(fileName, methodName, selectGroupTable, SELECT));
                                         }
                                     }
@@ -155,22 +153,22 @@ public class DAOTableSv {
                             if (currentTable.contains(",")) {
                                 List<String> currentTableList = Arrays.asList(currentTable.split(","));
                                 for (int h = 0; h < currentTableList.size(); h++) {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTableList.get(h) + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTableList.get(h) +",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTableList.get(h), SELECT));
                                 }
                             } else {
-                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + currentTable + ";操作:select");
+                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+currentTable +",operation:"+SELECT);
                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, currentTable, SELECT));
                             }
                         } else {
                             if (simpleTable.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(simpleTable.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTableList.get(h) + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTableList.get(h) +",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTableList.get(h), SELECT));
                                 }
                             } else {
-                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTable + ";操作:select");
+                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTable +",operation:"+SELECT);
                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTable, SELECT));
                             }
                         }
@@ -181,22 +179,22 @@ public class DAOTableSv {
                             if (currentString.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(currentString.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTableList.get(h) + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTableList.get(h) +",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTableList.get(h), SELECT));
                                 }
                             } else {
-                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + sqlTrimString + ";操作:select");
+                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+sqlTrimString+",operation:"+SELECT);
                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, sqlTrimString, SELECT));
                             }
                         } else {
                             if (sqlTrimString.contains(",")) {
                                 List<String> simpleTableList = Arrays.asList(sqlTrimString.split(","));
                                 for (int h = 0; h < simpleTableList.size(); h++) {
-                                    System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + simpleTableList.get(h) + ";操作:select");
+                                    LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+simpleTableList.get(h)+",operation:"+SELECT);
                                     insertTableBeanSet.add(new InsertTableBean(fileName, methodName, simpleTableList.get(h), SELECT));
                                 }
                             } else {
-                                System.out.println("文件名:" + fileName + ";方法名:" + methodName + ";数据库表名:" + sqlTrimString + ";操作:select");
+                                LOGGER.info("fileName:"+fileName,",methodName:"+methodName+",tableName:"+sqlTrimString+",operation:"+SELECT);
                                 insertTableBeanSet.add(new InsertTableBean(fileName, methodName, sqlTrimString, SELECT));
                             }
                         }
