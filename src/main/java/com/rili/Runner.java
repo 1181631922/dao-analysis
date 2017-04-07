1ed532e36d0fc6d42f1517bd446eb435a63b79a6
chenyiming <chenyiming@365rili.com>
2017-04-07 11:38:22 +0800

    test codacy

--- src/main/java/com/rili/Runner.java
+++ src/main/java/com/rili/Runner.java
@@ -36,51 +36,51 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
 
-//        boolean doAnalysis = false;
-//        boolean doTables = false;
-//        String tempStr;
-//        String tempStr2;
-//        String tables = null;
-//        List<String> arguments = Lists.newArrayList(strings);
-//        for (String argument : arguments) {
-//            LOGGER.info("argument:{}", argument);
-//        }
-//        if (arguments.isEmpty() || arguments.size() > 2) {
-//            // 参数报错
-//            LOGGER.error("please run with correct arguments...");
-//        } else if (arguments.size() == 1) {
-//            tempStr = StringUtils.replaceAll(arguments.get(0).trim(), " ", "");
-//            if (Constant.DO_ANALYSIS.equals(tempStr)) {
-//                doAnalysis = true;
-//            } else if (StringUtils.indexOf(tempStr, Constant.TABLES) == 0) {
-//                doTables = true;
-//                tables = StringUtils.substring(tempStr, StringUtils.indexOf(tempStr, "=") + 1);
-//            }
-//        } else {
-//            tempStr = StringUtils.replaceAll(arguments.get(0).trim(), " ", "");
-//            tempStr2 = StringUtils.replaceAll(arguments.get(1).trim(), " ", "");
-//            if ((Constant.DO_ANALYSIS.equals(tempStr) && StringUtils.indexOf(tempStr2, Constant.TABLES) == 0) ||
-//                    (Constant.DO_ANALYSIS.equals(tempStr2) && StringUtils.indexOf(tempStr, Constant.TABLES) == 0)) {
-//                doAnalysis = true;
-//                doTables = true;
-//
-//                if (StringUtils.indexOf(tempStr2, Constant.TABLES) == 0) {
-//                    tables = StringUtils.substring(tempStr2, StringUtils.indexOf(tempStr2, "=") + 1);
-//                } else {
-//                    tables = StringUtils.substring(tempStr, StringUtils.indexOf(tempStr, "=") + 1);
-//                }
-//            }
-//        }
-//
-//        if (doAnalysis && doTables) {
-//            doAnalysis();
-//            doTables(tables);
-//
-//        } else if (doAnalysis) {
-//            doAnalysis();
-//        } else if (doTables) {
-//            doTables(tables);
-//        }
+        boolean doAnalysis = false;
+        boolean doTables = false;
+        String tempStr;
+        String tempStr2;
+        String tables = null;
+        List<String> arguments = Lists.newArrayList(strings);
+        for (String argument : arguments) {
+            LOGGER.info("argument:{}", argument);
+        }
+        if (arguments.isEmpty() || arguments.size() > 2) {
+            // 参数报错
+            LOGGER.error("please run with correct arguments...");
+        } else if (arguments.size() == 1) {
+            tempStr = StringUtils.replaceAll(arguments.get(0).trim(), " ", "");
+            if (Constant.DO_ANALYSIS.equals(tempStr)) {
+                doAnalysis = true;
+            } else if (StringUtils.indexOf(tempStr, Constant.TABLES) == 0) {
+                doTables = true;
+                tables = StringUtils.substring(tempStr, StringUtils.indexOf(tempStr, "=") + 1);
+            }
+        } else {
+            tempStr = StringUtils.replaceAll(arguments.get(0).trim(), " ", "");
+            tempStr2 = StringUtils.replaceAll(arguments.get(1).trim(), " ", "");
+            if ((Constant.DO_ANALYSIS.equals(tempStr) && StringUtils.indexOf(tempStr2, Constant.TABLES) == 0) ||
+                    (Constant.DO_ANALYSIS.equals(tempStr2) && StringUtils.indexOf(tempStr, Constant.TABLES) == 0)) {
+                doAnalysis = true;
+                doTables = true;
+
+                if (StringUtils.indexOf(tempStr2, Constant.TABLES) == 0) {
+                    tables = StringUtils.substring(tempStr2, StringUtils.indexOf(tempStr2, "=") + 1);
+                } else {
+                    tables = StringUtils.substring(tempStr, StringUtils.indexOf(tempStr, "=") + 1);
+                }
+            }
+        }
+
+        if (doAnalysis && doTables) {
+            doAnalysis();
+            doTables(tables);
+
+        } else if (doAnalysis) {
+            doAnalysis();
+        } else if (doTables) {
+            doTables(tables);
+        }
 
         Gson gson = new Gson();
         List<RelationBean> result = Lists.newArrayList();
